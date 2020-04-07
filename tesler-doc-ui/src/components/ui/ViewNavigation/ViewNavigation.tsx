39b8bfb82c57e2ac32abc69e1d8a5849c94280f6
kdergachev <dergachev@hotmail.com>
2020-04-07 04:16:04 +0300

    upgrade tesler-ui 1.8.2 -> 1.11.0, refactor navigation to 2.0.0 structure through  `useViewTabs` hook

--- tesler-doc-ui/src/components/ui/ViewNavigation/ViewNavigation.tsx
+++ tesler-doc-ui/src/components/ui/ViewNavigation/ViewNavigation.tsx
@@ -17,37 +17,23 @@
 
 import React from 'react'
 import {Tabs} from 'antd'
-import {historyObj, isViewNavigationGroup} from '@tesler-ui/core'
-import {ViewMetaResponse} from '@tesler-ui/core/interfaces/view'
-import {ViewNavigationItem as CoreViewNavigationItem} from '@tesler-ui/core/interfaces/navigation'
-import {ViewNavigationGroup} from '@tesler-ui/core/interfaces/navigation'
+import {historyObj, useViewTabs} from '@tesler-ui/core'
 import styles from './ViewNavigation.less'
 
-export interface ViewNavigationProps {
-    views: ViewNavigationItem[],
-    activeView: string
-}
-
-export interface ViewNavigationItem {
-    id: number,
-    title: string,
-    url: string
-}
-
-export function ViewNavigation(props: ViewNavigationProps) {
-
+export function ViewNavigation() {
+    const tabs = useViewTabs(1)
     const handleChange = (key: string) => {
         historyObj.push(key)
     }
 
     return <nav className={styles.container}>
         <Tabs
-            activeKey={props.activeView}
+            activeKey={tabs?.find(item => item.selected)?.url}
             tabBarGutter={24}
             size="large"
             onChange={handleChange}
         >
-            {props.views.map((item) =>
+            {tabs.map(item =>
                 <Tabs.TabPane
                     key={item.url}
                     tab={<span className={styles.item}>
@@ -58,37 +44,6 @@ export function ViewNavigation(props: ViewNavigationProps) {
             )}
         </Tabs>
     </nav>
-}
-
-/**
- * TODO: JSDOC
- */
-export function useViewNavigation(
-    menu: ViewNavigationGroup[],
-    views: ViewMetaResponse[]
-): ViewNavigationItem[] {
-    return React.useMemo(() => {
-        if (!menu || !views) {
-            return []
-        }
-
-        return menu[0].child.map(item => {
-            let title = ''
-            let viewName = ''
-            if (isViewNavigationGroup(item)) {
-                title = item.title
-                viewName = item.child && item.child[0] && (item.child[0] as CoreViewNavigationItem).viewName
-            } else {
-                viewName = (item as CoreViewNavigationItem).viewName
-            }
-            const matchingView = views.find(view => view.name === viewName)
-            return matchingView && {
-                id: matchingView.id,
-                title: title || matchingView.title,
-                url: matchingView.url
-            }
-        }).filter(item => !!item)
-    }, [menu, views])
 }
 
 export default React.memo(ViewNavigation)
