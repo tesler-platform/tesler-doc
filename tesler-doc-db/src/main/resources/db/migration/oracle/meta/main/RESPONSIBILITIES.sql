3ae153bda95375d8dae3d21d66ad7cf43000dc5e
kdergachev <dergachev@hotmail.com>
2020-04-20 20:36:48 +0300

    draft

--- tesler-doc-db/src/main/resources/db/migration/oracle/meta/main/RESPONSIBILITIES.sql
+++ tesler-doc-db/src/main/resources/db/migration/oracle/meta/main/RESPONSIBILITIES.sql
@@ -12,6 +12,30 @@ insert into responsibilities (id, resp_type, screens, dept_id, internal_role_cd)
       "text": "Example",
       "url": "/screen/example",
       "defaultScreen": true
+    },
+    {
+      "name": "tutorial",
+      "text": "Tutorial",
+      "icon": "book",
+      "url": "/screen/tutorial"
+    },
+    {
+      "name": "components",
+      "text": "Components Overview",
+      "icon": "block",
+      "url": "/screen/components"
+    },
+    {
+      "name": "features",
+      "text": "Features",
+      "icon": "star",
+      "url": "/screen/features"
+    },
+    {
+      "name": "api-reference",
+      "text": "API Reference",
+      "icon": "api",
+      "url": "/screen/api-reference"
     }
     ]', 0 , 'ADMIN');
 insert into responsibilities (id, resp_type, dept_id, internal_role_cd, responsibilities) VALUES (nextval('RESPONSIBILITY_ID'), 'VIEW', 0, 'ADMIN', 'bankcard');
