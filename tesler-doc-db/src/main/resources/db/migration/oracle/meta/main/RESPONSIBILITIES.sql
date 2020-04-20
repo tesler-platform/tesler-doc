1c4962561e5d77acc19591050184578e1a097b91
kdergachev <dergachev@hotmail.com>
2020-04-20 21:45:53 +0300

    add `getting-started` screen

--- tesler-doc-db/src/main/resources/db/migration/oracle/meta/main/RESPONSIBILITIES.sql
+++ tesler-doc-db/src/main/resources/db/migration/oracle/meta/main/RESPONSIBILITIES.sql
@@ -1,17 +1,10 @@
 insert into responsibilities (id, resp_type, screens, dept_id, internal_role_cd) VALUES (nextval('RESPONSIBILITY_ID'), 'SCREEN',
-'[{
-      "id": "id2",
-      "name": "doc",
-      "text": "Documentation",
-      "url": "/screen/doc",
-      "defaultScreen": true
-    },
+'[
     {
-      "id": "id3",
-      "name": "example",
-      "text": "Example",
-      "url": "/screen/example",
-      "defaultScreen": true
+      "name": "getting-started",
+      "text": "Getting Started",
+      "icon": "rocket",
+      "url": "/screen/getting-started"
     },
     {
       "name": "tutorial",
@@ -36,6 +29,20 @@ insert into responsibilities (id, resp_type, screens, dept_id, internal_role_cd)
       "text": "API Reference",
       "icon": "api",
       "url": "/screen/api-reference"
+    },
+    {
+      "id": "id2",
+      "name": "doc",
+      "text": "Documentation",
+      "url": "/screen/doc",
+      "defaultScreen": true
+    },
+    {
+      "id": "id3",
+      "name": "example",
+      "text": "Example",
+      "url": "/screen/example",
+      "defaultScreen": true
     }
     ]', 0 , 'ADMIN');
 insert into responsibilities (id, resp_type, dept_id, internal_role_cd, responsibilities) VALUES (nextval('RESPONSIBILITY_ID'), 'VIEW', 0, 'ADMIN', 'bankcard');
